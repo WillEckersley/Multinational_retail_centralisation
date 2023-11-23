000d1875f402356bae2d7a22dcd2568005e107b6
WillEckersley <will_eckersley@hotmail.co.uk>
2023-11-23 21:12:58 +0000

    Clead join_date column

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,1 +1,20 @@
+import pandas as pd
+import data_extraction as dex
+import database_utils as dbu
+
+from datetime import datetime
+
+
 class DataCleaner:
+
+    engine = dbu.DatabaseConnector().init_db_engine()
+
+    def clean_user_data(self):
+        legacy = dex.DataExtractor().read_rds_tables(DataCleaner.engine, "legacy_users")
+        legacy["join_date"] = pd.to_datetime(legacy["join_date"], errors="coerce", format="%Y-%m-%d")
+        legacy = legacy.dropna() # Drop 59 NaT columns in the 'join_date' column 
+        legacy.info()
+        print(legacy.head())
+
+x = DataCleaner()
+x.clean_user_data()
