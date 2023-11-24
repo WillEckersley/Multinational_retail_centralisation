ed03dae8bcf2dd7957868b929c72bcc1918f15e5
WillEckersley <will_eckersley@hotmail.co.uk>
2023-11-24 17:27:54 +0000

    Further columns cleaned

--- data_cleaning.py
+++ data_cleaning.py
@@ -12,9 +12,12 @@ class DataCleaner:
     def clean_user_data(self):
         legacy = dex.DataExtractor().read_rds_tables(DataCleaner.engine, "legacy_users")
         legacy["join_date"] = pd.to_datetime(legacy["join_date"], errors="coerce", format="%Y-%m-%d")
-        legacy = legacy.dropna() # Drop 59 NaT columns in the 'join_date' column 
-        legacy.info()
-        print(legacy.head())
-
+        legacy = legacy.dropna()                                                                        # Drop 59 NaT columns in the 'join_date' column. 
+        legacy["country_code"] = legacy["country_code"].replace("GGB", "GB")                            # replace 6 eroneous instances of 'GGB' in country_code column with 'GB'.
+        legacy = legacy.drop(["country"], axis="columns")                                               # drop 'country' column in favour of country_code as less prone to error and to prevent needless redundancy.
+        legacy = legacy.rename(columns={"country_code": "country"})                                     # rename 'country_code' column to 'country' for simplicity.
+        legacy["address"] = legacy["address"].str.split("\\n")                                          # split the corupted addresses at the point '\n'.
+        legacy["address"] = legacy["address"].apply(lambda x: ", ".join(x))                             # join the lists from previous line into strings by applying a join to the lists. 
+        print(legacy["address"].head(30))
 x = DataCleaner()
 x.clean_user_data()
