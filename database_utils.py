f519b0a229db094ca383c4d7c2f378beac972c23
WillEckersley <will_eckersley@hotmail.co.uk>
2023-11-29 18:48:30 +0000

    Add db_upload method

--- database_utils.py
+++ database_utils.py
@@ -14,7 +14,6 @@ class DatabaseConnector:
         with open("/Users/willeckersley/projects/repositories/Multinational_retail_centralisation/db_creds.yaml", "r") as f:
             credentials = yaml.safe_load(f)
             return credentials
-
     
     def init_db_engine(self):
         creds_dict = self.read_db_creds()
@@ -32,7 +31,16 @@ class DatabaseConnector:
     def list_db_tables(self):
         connection = self.init_db_engine()
 
-
-
-connector = DatabaseConnector()
-connector.init_db_engine()
+    def upload_to_db(self, df, name):
+        creds_dict = self.read_db_creds()
+        host = creds_dict["SALES_HOST"]
+        user = creds_dict["SALES_USER"]
+        password = creds_dict["SALES_PASSWORD"]
+        db = creds_dict["SALES_DATABASE"]
+        port = creds_dict["SALES_PORT"]
+        type = creds_dict["SALES_TYPE"]
+        api = creds_dict["SALES_API"]
+        with psycopg2.connect(host=host, user=user, password=password, database=db, port=port) as psy_connection:
+            engine = sqlalchemy.create_engine(f"{type}+{api}://{user}:{password}@{host}:{port}/{db}")
+            database_uploader = df.to_sql(name, engine, if_exists="replace")
+        return database_uploader
