0273819d45e7a00ab06c42bd40d04be7b07fb8c2
WillEckersley <will_eckersley@hotmail.co.uk>
2023-12-01 14:13:00 +0000

    PDF extraction method created

--- data_extraction.py
+++ data_extraction.py
@@ -1,34 +1,29 @@
 import pandas as pd
 import database_utils as dbu
 import sqlalchemy
+import tabula
 
 from sqlalchemy import text
 
 class DataExtractor:
 
 
-    def list_db_tables(self):
-        engine_obj = dbu.DatabaseConnector()
-        engine = engine_obj.init_db_engine()
+    engine = dbu.DatabaseConnector().init_db_engine()
+
+
+    def list_db_tables(self, engine):
         with engine.execution_options(isolation_level="AUTOCOMMIT").connect() as connection:
             table_names_data = connection.execute(text("""SELECT table_name 
                                                         FROM information_schema.tables
                                                         WHERE table_schema = 'public'"""))
-            table_name_lst = [table_name for table_name in table_names_data]
-            return table_name_lst
+            return [table_name for table_name in table_names_data]
                 
-
-    def read_rds_tables(self, table_name):
-        engine_obj = dbu.DatabaseConnector()
-        engine = engine_obj.init_db_engine()
-        table_names = self.list_db_tables()
-        for table in table_names:
+    def read_rds_tables(self, engine, table_name):
+        table_names = self.list_db_tables(engine)
+        for _ in table_names:
             result = pd.read_sql_table(table_name, engine)
-        print(result.head())
-
-        
-x = DataExtractor()
-x.list_db_tables()
-x.read_rds_tables("legacy_users")
-x.read_rds_tables("orders_table")
-x.read_rds_tables("legacy_store_details")
+        return result
+    
+    def retrieve_pdf_data(self, link):
+        pdf_data_read = tabula.read_pdf(link, pages="all")
+        return pdf_data_read
