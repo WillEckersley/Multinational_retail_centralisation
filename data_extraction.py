ec7284782c447598f6f01e27d6cd63285054546b
WillEckersley <will_eckersley@hotmail.co.uk>
2023-12-05 16:25:33 +0000

    Store number list method added

--- data_extraction.py
+++ data_extraction.py
@@ -2,14 +2,16 @@ import pandas as pd
 import database_utils as dbu
 import sqlalchemy
 import tabula
+import requests
+import json
 
 from sqlalchemy import text
 
+store_endpoint = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details"
+json_keys = "/Users/willeckersley/projects/repositories/Multinational_retail_centralisation/api_key.json"
+number_endpoint = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores/"
+
 class DataExtractor:
-
-
-    engine = dbu.DatabaseConnector().init_db_engine()
-
 
     def list_db_tables(self, engine):
         with engine.execution_options(isolation_level="AUTOCOMMIT").connect() as connection:
@@ -27,3 +29,10 @@ class DataExtractor:
     def retrieve_pdf_data(self, link):
         pdf_data_read = tabula.read_pdf(link, pages="all")
         return pdf_data_read
+
+    def list_number_of_stores(self, endpoint, header={"x-api-key" : "yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX"}):
+            response = requests.get(endpoint, headers=header)
+            print(response.json())
+
+x = DataExtractor()
+x.list_number_of_stores(number_endpoint)
