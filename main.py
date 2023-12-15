f80b8255b5fd542685ad13c865814acf0b2bd1f6
WillEckersley <will_eckersley@hotmail.co.uk>
2023-12-15 16:03:50 +0000

    Add success message

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import database_utils as dbu
 
 if __name__ == "__main__":
     database_connector = dbu.DatabaseConnector()
-    
+    database_connector.upload_to_db(dcl.DataCleaner().clean_user_data(), "dim_users")
+    database_connector.upload_to_db(dcl.DataCleaner().clean_card_data(), "dim_card_details")
     database_connector.upload_to_db(dcl.DataCleaner().clean_store_data(), "dim_store_details")
     database_connector.upload_to_db(dcl.DataCleaner().clean_products_data(), "dim_products")
     database_connector.upload_to_db(dcl.DataCleaner().clean_orders_table(), "orders_table")
     database_connector.upload_to_db(dcl.DataCleaner().clean_purchase_dates(), "dim_date_times")
+    print("Upload successful")
