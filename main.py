f22061007d1bf0177b102037be4108ab21b40648
WillEckersley <will_eckersley@hotmail.co.uk>
2024-02-10 12:21:12 +0000

    Refactoring

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import database_utils as dbu
 
 if __name__ == "__main__":
     database_connector = dbu.DatabaseConnector()
-    database_connector.upload_to_db(dcl.DataCleaner().clean_user_data(), "dim_users")
-    database_connector.upload_to_db(dcl.DataCleaner().clean_card_data(), "dim_card_details")
-    database_connector.upload_to_db(dcl.DataCleaner().clean_store_data(), "dim_store_details")
-    database_connector.upload_to_db(dcl.DataCleaner().clean_products_data(), "dim_products")
-    database_connector.upload_to_db(dcl.DataCleaner().clean_orders_table(), "orders_table")
-    database_connector.upload_to_db(dcl.DataCleaner().clean_purchase_dates(), "dim_date_times")
+    data_cleaner = dcl.DataCleaner()
+    database_connector.upload_to_db(data_cleaner.clean_user_data(), "dim_users")
+    database_connector.upload_to_db(data_cleaner.clean_card_data(), "dim_card_details")
+    database_connector.upload_to_db(data_cleaner.clean_store_data(), "dim_store_details")
+    database_connector.upload_to_db(data_cleaner.clean_products_data(), "dim_products")
+    database_connector.upload_to_db(data_cleaner.clean_orders_table(), "orders_table")
+    database_connector.upload_to_db(data_cleaner.clean_purchase_dates(), "dim_date_times")
     print("Upload successful")
